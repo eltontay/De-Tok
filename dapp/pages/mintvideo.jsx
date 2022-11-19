6e451f454f61d0a62cee012a5b7421a0e8efe243
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-11-19 21:37:04 +0900

    upload metadata

--- dapp/pages/mintvideo.jsx
+++ dapp/pages/mintvideo.jsx
@@ -8,25 +8,31 @@ function makeStorageClient() {
   return new Web3Storage({ token: process.env.NEXT_PUBLIC_WEB3STORAGE_TOKEN });
 }
 
-const StoreContent = async (files) => {
+const storeContent = async (files) => {
   const client = makeStorageClient();
   const cid = await client.put(files);
   console.log("stored files with cid:", cid);
+  // for what?
   setTimeout(3000);
   return cid;
 };
 
+const makeJsonFileObject = (json) => {
+  const blob = new Blob([JSON.stringify(json)], { type: "application/json" });
+  return new File([blob], "metadata.json");
+};
+
 const hoge = async () => {
   // const cid = await storage.put(files,options);
+  console.log("get files");
   const storage = makeStorageClient();
-  const cid = "bafybeigkdrxadzsbdeyomforg7yzvm2wrz35clnp6ifnwxxhwjohz7xvpm";
-
-  // const resdata = await storage.get(cid);
-  // const filesReterived = await resdata.files();
-  // for (const file of filesReterived) {
-  //   console.log(file);
-  //   console.log(`${file.cid} ${file.name} ${file.size}`);
-  // }
+  const cid = "bafybeidtig7gruy5yirxjhbp675apd3qkrr6soawhh7bhpj7l4sdp7pawe";
+  const resdata = await storage.get(cid);
+  const filesReterived = await resdata.files();
+  for (const file of filesReterived) {
+    console.log(file);
+    console.log(`${file.cid} ${file.name} ${file.size}`);
+  }
   const res = await storage.status(cid);
   console.log(res);
 };
@@ -37,14 +43,21 @@ export default function MintVideo() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [author, setAuthor] = useState("");
+  const [video, setVideo] = useState();
 
-  const onSubmit = () => {
-    console.log("onSubmit");
+  const onSubmit = async () => {
+    if (!video) {
+      return;
+    }
+    console.log("uploading...");
+    const jsonFile = makeJsonFileObject({ title, description, author });
+    console.log(jsonFile);
+    console.log(video);
+    const cid = await storeContent([video, jsonFile]);
   };
   const onDrop = useCallback(async (acceptedFiles) => {
-    console.log("uploading...");
-    const cid = await StoreContent(acceptedFiles);
-    console.log("cid:", cid);
+    // const cid = await storeContent(acceptedFiles);
+    setVideo(acceptedFiles[0]);
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
@@ -99,17 +112,29 @@ export default function MintVideo() {
           </label>
           <label className="w-full font-bold pt-2">
             Video
-            <div
-              {...getRootProps()}
-              className="h-40 cursor-pointer border border-dashed border-white flex items-center justify-center"
-            >
-              <input {...getInputProps()} />
-              <div className="px-4">
-                {isDragActive
-                  ? "Drop the files here ..."
-                  : "Drag & drop some files here, or click to select files"}
+            {video ? (
+              <div>
+                <span className="pr-4">{video.name}</span>
+                <button
+                  className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  onClick={() => setVideo(null)}
+                >
+                  Remove
+                </button>
               </div>
-            </div>
+            ) : (
+              <div
+                {...getRootProps()}
+                className="h-40 cursor-pointer border border-dashed border-white flex items-center justify-center"
+              >
+                <input {...getInputProps()} />
+                <div className="px-4">
+                  {isDragActive
+                    ? "Drop the files here ..."
+                    : "Drag & drop some files here, or click to select files"}
+                </div>
+              </div>
+            )}
           </label>
           <div className="pt-6">
             <button
