c71a220b09f4258d13bdb5b841496ce0400a2abe
Elton <57287843+eltontay@users.noreply.github.com>
2022-11-20 16:42:33 +0800

    changing button to useeffect

--- dapp/components/BasicVideos.jsx
+++ dapp/components/BasicVideos.jsx
@@ -1,48 +1,45 @@
-import React, { useState, useCallback, useEffect } from "react";
-import styles from "../styles/BasicVideos.module.css";
+import React, { useState, useCallback, useEffect } from 'react';
+import styles from '../styles/BasicVideos.module.css';
 
 import { useAccount, useContractRead, useProvider, useSigner } from 'wagmi';
-import {
-  DETOK_ABI,
-  DeTok_Contract_Address
-} from '../constants/constants';
+import { DETOK_ABI, DeTok_Contract_Address } from '../constants/constants';
 
+import VideoGallery from './VideoGallery';
 
 export const BasicVideos = () => {
-  const [status, setstatus] = useState("");
+  const [status, setstatus] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [videoIds, setVideoIds] = useState([]);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
-    const { refetch } = useContractRead(
-      {
-        address: DeTok_Contract_Address,
-        abi:DETOK_ABI,
-        functionName: 'getAllBasicCid',
-      },
-    )
-    
-    const handleClick = async () => {
+  const { refetch } = useContractRead({
+    address: DeTok_Contract_Address,
+    abi: DETOK_ABI,
+    functionName: 'getAllBasicCid',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
       const res = await refetch();
-      const { status, data} = res;
-      if(res.status == "success"){
+      const { status, data } = res;
+      if (res.status == 'success') {
         setVideoIds(data);
       }
-    }
+      console.log(data);
+    };
+    fetchData();
+  }, [videoIds]);
 
-    return (
+  return (
+    <div className="pt-2">
       <div className="pt-2">
-         <div className="pt-2">
-          <div className={styles.title}>
-          <span className={`${styles.title.description}`}>
-               Basic Videos
-            </span>
-          </div>
-          <div>
-            {videoIds}
-          </div>
+        <label>{videoIds}</label>
+        <div>
+          <VideoGallery data={{ videoIds }}></VideoGallery>
+        </div>
+        <div>{videoIds}</div>
       </div>
-      </div>
-    )
-}
+    </div>
+  );
+};
--- dapp/components/TrendingVideos.jsx
+++ dapp/components/TrendingVideos.jsx
@@ -1,58 +1,72 @@
-import React, { useState, useCallback, useEffect } from "react";
-import { useAccount, useContractRead, useProvider, useSigner } from 'wagmi';
-
-import VideoGallery from "./VideoGallery";
-
-import styles from "../styles/TrendingVideos.module.css";
-
+import React, { useState, useCallback, useEffect, useContext } from 'react';
 import {
-  DETOK_ABI,
-  DeTok_Contract_Address
-} from '../constants/constants';
+  useAccount,
+  useContractRead,
+  useContractInfiniteReads,
+  useProvider,
+  useSigner,
+} from 'wagmi';
 
+import VideoGallery from './VideoGallery';
+
+import styles from '../styles/TrendingVideos.module.css';
+
+import { DETOK_ABI, DeTok_Contract_Address } from '../constants/constants';
 
 export const TrendingVideos = () => {
-  const [status, setstatus] = useState("");
+  const [status, setstatus] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [videoIds, setVideoIds] = useState([]);
   const [cids, setCids] = useState([]);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
+  const { refetch } = useContractRead({
+    address: DeTok_Contract_Address,
+    abi: DETOK_ABI,
+    functionName: 'getAllTrendingCid',
+  });
 
-    const { refetch } = useContractRead(
-      {
-        address: DeTok_Contract_Address,
-        abi:DETOK_ABI,
-        functionName: 'getAllTrendingCid',
-      },
-    )
-        
-    const handleClick = async () => {
+  useEffect(() => {
+    const fetchData = async () => {
       const res = await refetch();
-      const { status, data} = res;
-      if(res.status == "success"){
+      const { status, data } = res;
+      if (res.status == 'success') {
         setVideoIds(data);
-        setCids(["bafybeicq7qd5ns67yfnwfclhunwifsgqptcddvhdfklnz5deopa4apr4qu","bafybeigg6vuimb2rwgmc3pfcrvwrzgg5gxjd75z74avvwxeaz47gsmwyx4"] )
       }
-    }
+      console.log(data);
+    };
+    fetchData();
+  }, [videoIds]);
 
-    return (
-      <div className="pt-2">
-          <div className={styles.title}>
-              <span className={`${styles.title.description}`}>
-                Trending Videos
-              </span>
-          </div>
-          <label>{cids}</label>
-          <div>
-             <VideoGallery data={{cids}}></VideoGallery>
-          </div>
-          <button
-          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow marign px-2"
-          onClick={() => handleClick()}
-        >
-         test  
-        </button>
+  // useEffect(async () => {
+  //   const res = await refetch();
+
+  // });
+
+  // const handleClick = async () => {
+  //   const res = await refetch();
+  //   const { status, data} = res;
+  //   if(res.status == "success"){
+  //     setVideoIds(data);
+  //     setCids(["bafybeicq7qd5ns67yfnwfclhunwifsgqptcddvhdfklnz5deopa4apr4qu","bafybeigg6vuimb2rwgmc3pfcrvwrzgg5gxjd75z74avvwxeaz47gsmwyx4"] )
+  //   }
+  // }
+
+  return (
+    <div className="pt-2">
+      <div className={styles.title}>
+        <span className={`${styles.title.description}`}>Trending Videos</span>
       </div>
-    )
-}
+      <label>{videoIds}</label>
+      <div>
+        <VideoGallery data={{ cids }}></VideoGallery>
+      </div>
+      <button
+        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow marign px-2"
+        onClick={() => handleClick()}
+      >
+        test
+      </button>
+    </div>
+  );
+};
