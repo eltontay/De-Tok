a02b6f6fe080271e10df235e3765ea7e940e8aef
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-11-20 22:18:12 +0900

    fetch balance

--- dapp/components/CheckBalance.jsx
+++ dapp/components/CheckBalance.jsx
@@ -1,21 +1,23 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { useAccount, useContractRead, usePrepareContractWrite, useProvider, useSigner } from 'wagmi';
-import styles from "../styles/CheckBalance.module.css"
-
-import { ClaimTokens  } from "./ClaimTokens";
-
 import {
-  DTOK_ABI,
-  DTok_Contract_Address,
-} from '../constants/constants';
+  useAccount,
+  useContractRead,
+  usePrepareContractWrite,
+  useProvider,
+  useSigner,
+} from "wagmi";
+import styles from "../styles/CheckBalance.module.css";
+import { utils, BigNumber } from "ethers";
+import { ClaimTokens } from "./ClaimTokens";
 
+import { DTOK_ABI, DTok_Contract_Address } from "../constants/constants";
 
 export const CheckBalance = () => {
   const [status, setstatus] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [canFetchBalance, setCanFetchBalance] = useState(false);
-  const [balance, setBalance] = useState(0);
+  // const [balance, setBalance] = useState(0);
   const [readBalance, setReadBalance] = useState(false);
   const [canMint, setCanMint] = useState(false);
   const [error, setError] = useState("");
@@ -24,57 +26,46 @@ export const CheckBalance = () => {
   const { data: signer } = useSigner();
   const { address, isConnected } = useAccount();
 
-  
-  const { refetch } = useContractRead(
-    {
-      address: DTok_Contract_Address,
-      abi:DTOK_ABI,
-      functionName: 'balanceOf',
-      args: [address],
-    },
-  )
-  
-  const handleClick = async () => {
-    const res = await refetch();
-    const { status, data} = res;
-    if(res.status == "success"){
-      setBalance(Number(data));
-      setReadBalance(true);
-    }
-  }
+  const {
+    data: balance,
+    // isError,
+    // isLoading,
+  } = useContractRead({
+    address: DTok_Contract_Address,
+    abi: DTOK_ABI,
+    functionName: "balanceOf",
+    args: [address],
+  });
 
   useEffect(() => {
     if (isConnected) {
       setCanFetchBalance(true);
     } else {
-      setIsError(true)
+      setIsError(true);
       setError("Connect your wallet first");
     }
 
-    if(readBalance & isConnected){
-      if(balance==0){
+    if (readBalance & isConnected) {
+      if (balance == 0) {
         setCanMint(true);
       }
     }
   });
 
-
   return (
     <div>
       <div className={styles.container}>
-        <button disabled={!canFetchBalance} hidden={!canFetchBalance}
+        <button
+          disabled={!canFetchBalance}
+          hidden={!canFetchBalance}
           className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow marign px-2"
-          onClick={() => handleClick()}
         >
-          Available DToks:{balance}
+          Available DToks:
+          {balance && utils.formatEther(BigNumber.from(balance))}
         </button>
-          { canMint ? (
-                  <ClaimTokens/>
-                ) : null }
-          { isError ? (
-            <label>{error}</label>
-          ) : null }
+        {canMint ? <ClaimTokens /> : null}
+        {isError ? <label>{error}</label> : null}
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
