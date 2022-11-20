1d0250f3979e18a46f4043cf90a30dce526829df
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-11-20 18:15:46 +0900

    temp

--- dapp/components/VideoComponent.jsx
+++ dapp/components/VideoComponent.jsx
@@ -2,21 +2,21 @@ import React, { useState, useCallback, useEffect, Children } from "react";
 
 // video src
 // video type
-// Image to show 
+// Image to show
 // storage deal info
-
+const IMG_SIZE = 200;
 export const VideoComponent = ({ src }) => {
-    const { url, type } = src;
-    // const demostr = `{"source": [{"src": "https://bafybeidtig7gruy5yirxjhbp675apd3qkrr6soawhh7bhpj7l4sdp7pawe.ipfs.w3s.link/ipfs/bafybeidtig7gruy5yirxjhbp675apd3qkrr6soawhh7bhpj7l4sdp7pawe/sample-5s.mp4", "type":"video/mp4"}], "attributes": {"preload": false, "playsinline": true, "controls": true}}`
-    const dataSrc = `{"source": [{"src":"${url}","type":"${ type}"}], "attributes": {"preload": false, "playsinline": true, "controls": true}}`
-    return (
-      <a
-        className="gallery-item" 
-        data-lg-size="1406-1390"
-        data-video={dataSrc}
-        data-sub-html=""
-        >
-         <img alt="img1" src="http://placekitten.com/g/100/100" />
-      </a>
-    )
-}
+  const { url, type } = src;
+  // const demostr = `{"source": [{"src": "https://bafybeidtig7gruy5yirxjhbp675apd3qkrr6soawhh7bhpj7l4sdp7pawe.ipfs.w3s.link/ipfs/bafybeidtig7gruy5yirxjhbp675apd3qkrr6soawhh7bhpj7l4sdp7pawe/sample-5s.mp4", "type":"video/mp4"}], "attributes": {"preload": false, "playsinline": true, "controls": true}}`
+  const dataSrc = `{"source": [{"src":"${url}","type":"${type}"}], "attributes": {"preload": false, "playsinline": true, "controls": true}}`;
+  return (
+    <a
+      className="gallery-item cursor-pointer"
+      data-lg-size="1406-1390"
+      data-video={dataSrc}
+      data-sub-html=""
+    >
+      <img width={IMG_SIZE} alt="img1" src="http://placekitten.com/g/200/200" />
+    </a>
+  );
+};
--- dapp/components/VideoGallery.jsx
+++ dapp/components/VideoGallery.jsx
@@ -1,14 +1,14 @@
-import LightGallery from 'lightgallery/react';
+import LightGallery from "lightgallery/react";
 
 // import styles
-import 'lightgallery/css/lightgallery.css';
-import 'lightgallery/css/lg-zoom.css';
-import 'lightgallery/css/lg-thumbnail.css';
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
 
 // import plugins if you need
-import lgThumbnail from 'lightgallery/plugins/thumbnail';
-import lgZoom from 'lightgallery/plugins/zoom';
-import lgVideo from 'lightgallery/plugins/video';
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import lgVideo from "lightgallery/plugins/video";
 
 // lightgallery styles
 import "lightgallery/css/lightgallery.css";
@@ -20,27 +20,32 @@ import "@videojs/themes/dist/fantasy/index.css";
 
 import styles from "../styles/VideoGallery.module.css";
 
-import { VideoComponent } from './VideoComponent';
-
+import { VideoComponent } from "./VideoComponent";
 
 export default function VideoGallery({ cids }) {
-    console.log(cids);
+  console.log(cids);
 
+  const url =
+    "https://bafybeidtig7gruy5yirxjhbp675apd3qkrr6soawhh7bhpj7l4sdp7pawe.ipfs.w3s.link/ipfs/bafybeidtig7gruy5yirxjhbp675apd3qkrr6soawhh7bhpj7l4sdp7pawe/sample-5s.mp4";
+  const type = "video/mp4";
 
-    const url= "https://bafybeidtig7gruy5yirxjhbp675apd3qkrr6soawhh7bhpj7l4sdp7pawe.ipfs.w3s.link/ipfs/bafybeidtig7gruy5yirxjhbp675apd3qkrr6soawhh7bhpj7l4sdp7pawe/sample-5s.mp4";
-    const type = "video/mp4";
-
-    return (
-        <div className={styles.mediaGrid}>
-            <LightGallery
-            download={false}
-            speed={500}
-            plugins={[lgVideo]}
-            videojs={true}
-            videojsTheme="vjs-theme-fantasy"
-            >
-             <VideoComponent src={{ url, type}}></VideoComponent>     
-           </LightGallery>
-       </div>
-    );
+  return (
+    <div className="p-4">
+      <LightGallery
+        elementClassNames="flex flex-row flex-wrap gap-4"
+        download={false}
+        speed={500}
+        plugins={[lgVideo]}
+        videojs={true}
+        videojsTheme="vjs-theme-fantasy"
+      >
+        {/* {videos.map((video)=>{
+          <VideoComponent src={video.src} />
+        })}   */}
+        <VideoComponent src={{ url, type }}></VideoComponent>
+        <VideoComponent src={{ url, type }}></VideoComponent>
+        <VideoComponent src={{ url, type }}></VideoComponent>
+      </LightGallery>
+    </div>
+  );
 }
