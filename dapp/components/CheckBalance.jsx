7916f3a12b0fac8b5ec50ba137d9aa3f18227835
betharavikiran <betha.ravikiran@gmail.com>
2022-11-20 09:28:18 +0530

    integrated with user account

--- dapp/components/CheckBalance.jsx
+++ dapp/components/CheckBalance.jsx
@@ -15,51 +15,67 @@ export const CheckBalance = () => {
   const [status, setstatus] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [canFetchBalance, setCanFetchBalance] = useState(false);
   const [balance, setBalance] = useState(0);
   const [readBalance, setReadBalance] = useState(false);
   const [canMint, setCanMint] = useState(false);
   const [error, setError] = useState("");
 
-    const { refetch } = useContractRead(
-      {
-        address: DTok_Contract_Address,
-        abi:DTOK_ABI,
-        functionName: 'balanceOf',
-        args: ['0x9680A866299a8D021114Ac6A460Ba8e5860B68Fb'],
-      },
-    )
-    
-    const handleClick = async () => {
-      const res = await refetch();
-      const { status, data} = res;
-      if(res.status == "success"){
-        setBalance(Number(data));
-        setReadBalance(true);
-      }
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+  const { address, isConnected } = useAccount();
+
+  
+  const { refetch } = useContractRead(
+    {
+      address: DTok_Contract_Address,
+      abi:DTOK_ABI,
+      functionName: 'balanceOf',
+      args: [address],
+    },
+  )
+  
+  const handleClick = async () => {
+    const res = await refetch();
+    const { status, data} = res;
+    if(res.status == "success"){
+      setBalance(Number(data));
+      setReadBalance(true);
+    }
+  }
+
+  useEffect(() => {
+    if (isConnected) {
+      setCanFetchBalance(true);
+    } else {
+      setIsError(true)
+      setError("Connect your wallet first");
     }
 
-    useEffect(() => {
-      if(readBalance){
-        if(balance==0){
-          setCanMint(true);
-        }
+    if(readBalance & isConnected){
+      if(balance==0){
+        setCanMint(true);
       }
-    });
+    }
+  });
 
 
-    return (
-      <div>
-    <div className="pt-2">
-      <button
-        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-        onClick={() => handleClick()}
-      >
-       Available DToks:{balance}
-      </button>
-    </div>
-    { canMint ? (
-              <ClaimTokens/>
-            ) : null }
-    </div>
-    )
+  return (
+    <div>
+  <div className="pt-2">
+    <button disabled={!canFetchBalance} hidden={!canFetchBalance}
+      className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+      onClick={() => handleClick()}
+    >
+      Available DToks:{balance}
+    </button>
+  </div>
+  { canMint ? (
+            <ClaimTokens/>
+          ) : null }
+  { isError ? (
+     <label>{error}</label>
+  ) : null }
+  </div>
+  )
 }
